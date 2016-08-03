3d3ffec4f599574dd4db372a8eb1bd20deeb7ab8
proofit404 <proofit404@localhost.localdomain>
2016-08-03 19:20:05 +0300

    Focus XQuery result buffer by default.
    Make whole buffer readonly.  Bind bury-buffer to the q keybinding.
    
    Fix #5.

--- cider-any-uruk.el
+++ cider-any-uruk.el
@@ -65,21 +65,20 @@
 
 (defun cider-any-uruk-display-buffer (&rest content)
   "Show CONTENT in the buffer."
-  (let (points)
-    (with-current-buffer (generate-new-buffer cider-any-uruk-buffer-name)
-      (insert (car content))
-      (dolist (item (cdr content))
-        (insert "\n")
-        (insert (make-string 1 ?\))
-        (insert "\n")
-        (push (cons (- (point) 3) (point)) points)
-        (insert item))
-      (dolist (pos points)
-        (put-text-property (car pos) (cdr pos) 'read-only t))
-      (goto-char (point-min))
-      (normal-mode)
-      (page-break-lines-mode 1)
-      (display-buffer (current-buffer)))))
+  (pop-to-buffer
+   (with-current-buffer (generate-new-buffer cider-any-uruk-buffer-name)
+     (insert (car content))
+     (dolist (item (cdr content))
+       (insert "\n")
+       (insert (make-string 1 ?\))
+       (insert "\n")
+       (insert item))
+     (goto-char (point-min))
+     (normal-mode)
+     (page-break-lines-mode 1)
+     (read-only-mode 1)
+     (local-set-key (kbd "q") 'bury-buffer)
+     (current-buffer))))
 
 (defun cider-any-uruk (command &rest args)
   "Eval XQuery in Cider.
