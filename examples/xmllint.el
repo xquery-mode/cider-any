46638bf963fa729e18d3c772d7fb5687411ee831
Artem Malyshev <proofit404@gmail.com>
2016-09-06 21:44:11 +0300

    Remove debugging error message.

--- examples/xmllint.el
+++ examples/xmllint.el
@@ -4,18 +4,17 @@
   "Pretty print xml content first."
   (apply 'cider-any-uruk-display-buffer
 	 (mapcar
-	  (lambda (result) 
+	  (lambda (result)
 	    (with-temp-buffer
 	      (insert result)
 	      (normal-mode)
 	      (if (not (eq major-mode 'nxml-mode))
 		  result
-		(shell-command-on-region 
+		(shell-command-on-region
 		 (point-min) (point-max)
 		 "xmllint --format -" (buffer-name) t)
 		(indent-region (point-min) (point-max))
-		(buffer-substring-no-properties (point-min) (point-max))
-		(error "we are here"))))
+		(buffer-substring-no-properties (point-min) (point-max)))))
 	  content)))
 
 (setq cider-any-uruk-handler #'nxml-pprint-uruk)
