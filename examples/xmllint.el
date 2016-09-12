23e8ac03013dea8c922553cab31b850787cc7296
Artem Malyshev <proofit404@gmail.com>
2016-09-13 00:59:38 +0300

    Add xmllint-mode for toggle behavior.

--- examples/xmllint.el
+++ examples/xmllint.el
@@ -1,6 +1,18 @@
-;; Use xmllint to pretty print xml results.
+;;; xmllint.el --- XML results pretty printer.
 
-(defun nxml-pprint-uruk (&rest content)
+;;; Commentary:
+
+;;; Code:
+
+(require 'cider-any-uruk)
+
+(defgroup xmllint nil
+  "XML results pretty printer."
+  :group 'cider-any-uruk)
+
+(defvar xmllint-old-uruk-handler nil)
+
+(defun xmllint-pprint (&rest content)
   "Pretty print xml content first."
   (apply 'cider-any-uruk-display-buffer
 	 (mapcar
@@ -17,4 +29,18 @@
 		(buffer-substring-no-properties (point-min) (point-max)))))
 	  content)))
 
-(setq cider-any-uruk-handler #'nxml-pprint-uruk)
+;;;###autoload
+(define-minor-mode xmllint-mode
+  "XML results pretty printer."
+  :lighter nil
+  :group 'xmllint
+  :global t
+  (if xmllint-mode
+      (setq xmllint-old-uruk-handler cider-any-uruk-handler
+            cider-any-uruk-handler #'xmllint-pprint)
+    (setq cider-any-uruk-handler xmllint-old-uruk-handler
+          xmllint-old-uruk-handler nil)))
+
+(provide 'xmllint)
+
+;;; xmllint.el ends here
