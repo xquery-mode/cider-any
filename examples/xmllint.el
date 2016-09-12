a7201746a44a19e09ed690f47819154fc9773b6c
Artem Malyshev <proofit404@gmail.com>
2016-09-13 01:14:56 +0300

    Add pretty print indicator.

--- examples/xmllint.el
+++ examples/xmllint.el
@@ -10,24 +10,32 @@
   "XML results pretty printer."
   :group 'cider-any-uruk)
 
+(defcustom xmllint-indicator " •"
+  "String for pretty print indication.")
+
 (defvar xmllint-old-uruk-handler nil)
 
 (defun xmllint-pprint (&rest content)
   "Pretty print xml content first."
-  (apply 'cider-any-uruk-display-buffer
-	 (mapcar
-	  (lambda (result)
-	    (with-temp-buffer
-	      (insert result)
-	      (normal-mode)
-	      (if (not (eq major-mode 'nxml-mode))
-		  result
-		(shell-command-on-region
-		 (point-min) (point-max)
-		 "xmllint --format -" (buffer-name) t)
-		(indent-region (point-min) (point-max))
-		(buffer-substring-no-properties (point-min) (point-max)))))
-	  content)))
+  (let ((res (apply 'cider-any-uruk-display-buffer
+                    (mapcar
+                     (lambda (result)
+                       (with-temp-buffer
+                         (insert result)
+                         (normal-mode)
+                         (if (not (eq major-mode 'nxml-mode))
+                             result
+                           (shell-command-on-region
+                            (point-min) (point-max)
+                            "xmllint --format -" (buffer-name) t)
+                           (indent-region (point-min) (point-max))
+                           (buffer-substring-no-properties (point-min) (point-max)))))
+                     content))))
+    (when (bufferp res)
+      (with-current-buffer res
+        (setq mode-line-format
+              (cons '(:propertize xmllint-indicator face bold)
+                    mode-line-format))))))
 
 ;;;###autoload
 (define-minor-mode xmllint-mode
